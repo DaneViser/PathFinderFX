f0bc0508febda5cb01bdebcf205787f898da62fc
danijelrt7419 <danijelrt7419@gs.viser.edu.rs>
2022-08-13 14:15:22 +0200

    Added button to HelloApp and imports

--- src/main/java/com/example/pathfinderfx/HelloApplication.java
+++ src/main/java/com/example/pathfinderfx/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.pathfinderfx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -14,6 +15,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         GridPane grid = new GridPane();
         Scene scene1 = new Scene(grid);
+        Button bt1 = new Button();
         stage.setTitle("Hello!");
         stage.setScene(scene1);
         stage.show();
