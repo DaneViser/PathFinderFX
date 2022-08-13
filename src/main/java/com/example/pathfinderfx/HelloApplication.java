0a88aba72c854bdb8c3581ff43136be68be1b0b6
danijelrt7419 <danijelrt7419@gs.viser.edu.rs>
2022-08-13 14:32:08 +0200

    Added createRect function and listofRect attribute

--- src/main/java/com/example/pathfinderfx/HelloApplication.java
+++ src/main/java/com/example/pathfinderfx/HelloApplication.java
@@ -6,19 +6,37 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloApplication extends Application {
+    List<Rectangle> rectList = new ArrayList<>();
+    GridPane grid = new GridPane();
     @Override
     public void start(Stage stage) throws IOException {
-        GridPane grid = new GridPane();
-        Scene scene1 = new Scene(grid);
-        Button bt1 = new Button();
-        stage.setTitle("Hello!");
+        Scene scene1 = new Scene(grid,760,760);
+        createRectangles();
+        stage.setTitle("PathFinder");
         stage.setScene(scene1);
         stage.show();
+    }
+    private void createRectangles(){
+        for(int i = 0;i < 18;i++){
+            for(int j = 0;j < 18;j++){
+                Rectangle toAddRect = new Rectangle();
+                toAddRect.setHeight(40); toAddRect.setWidth(40);
+                toAddRect.setFill(Color.RED);
+                GridPane.setRowIndex(toAddRect, i);
+                GridPane.setColumnIndex(toAddRect, j);
+                grid.setVgap(2); grid.setHgap(2);
+                grid.getChildren().addAll(toAddRect);
+            }
+        }
     }
 
     public static void main(String[] args) {
