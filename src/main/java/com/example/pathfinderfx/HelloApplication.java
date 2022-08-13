905d7fc5f9887db361a6e9c77d439ec02af5c511
danijelrt7419 <danijelrt7419@gs.viser.edu.rs>
2022-08-13 16:17:48 +0200

    Added 'AddEventListenerFunction'

--- src/main/java/com/example/pathfinderfx/HelloApplication.java
+++ src/main/java/com/example/pathfinderfx/HelloApplication.java
@@ -1,9 +1,14 @@
 package com.example.pathfinderfx;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -21,16 +26,46 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         Scene scene1 = new Scene(grid,760,760);
         createRectangles();
+        AddEventListenerFunction();
         stage.setTitle("PathFinder");
         stage.setScene(scene1);
         stage.show();
     }
+    private void AddEventListenerFunction(){
+        // Adds eventListener on all rectangles
+        for(Rectangle rect: rectList){
+            rect.setOnMousePressed(mouseEvent -> {
+                if(mouseEvent.isPrimaryButtonDown()){
+                    // Pink
+                    if(rect.getFill() == Color.LIGHTBLUE){
+                        rect.setFill(Color.PINK);
+                    }
+                    // Green {END}
+                    else if(rect.getFill() == Color.PINK){
+                        rect.setFill(Color.GREEN);
+                    }
+                    // Red {START}
+                    else if(rect.getFill() == Color.GREEN){
+                        rect.setFill(Color.RED);
+                    }
+                    else // LightBlue
+                        if(rect.getFill() == Color.RED){
+                            rect.setFill(Color.LIGHTBLUE);
+                        }
+                }
+
+            });
+        }
+
+
+        }
+
     private void createRectangles(){
         for(int i = 0;i < 18;i++){
             for(int j = 0;j < 18;j++){
                 Rectangle toAddRect = new Rectangle();
                 toAddRect.setHeight(40); toAddRect.setWidth(40);
-                toAddRect.setFill(Color.RED);
+                toAddRect.setFill(Color.LIGHTBLUE);
                 GridPane.setRowIndex(toAddRect, i);
                 GridPane.setColumnIndex(toAddRect, j);
                 rectList.add(toAddRect);
